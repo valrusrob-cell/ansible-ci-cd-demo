3bf76bcd7d3c141747f85b9c3a7fd13991bcf1b9
valrusrob-cell <valrusrob@gmail.com>
2026-02-23 07:09:13 +0000

    ver2

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return f"""
+    <html>
+        <body>
+            <h1>Hello from Docker Container!</h1>
+            <p>Hostname: {socket.gethostname()}</p>
+            <p>Version: 1.0.0</p>
+        </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
