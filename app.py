cae8b6014e053ac42e01bbb51913ebd0550535d2
valrusrob-cell <valrusrob@gmail.com>
2026-02-23 08:28:38 +0000

    ver4

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello():
         <body>
             <h1>Hello from Docker Container!</h1>
             <p>Hostname: {socket.gethostname()}</p>
-            <p>Version: 3.0.0</p>
+            <p>Version: 4.0.0</p>
         </body>
     </html>
     """
